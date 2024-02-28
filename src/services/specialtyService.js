7ca8170c58ee0679d4cab8d36d6f42739da416e9
Phan Thac <phanthac.futa@gmail.com>
2024-02-28 21:40:37 +0700

    success code video 94

--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -54,7 +54,64 @@ let getAllSpecialty = () => {
     })
 }
 
+let getDetailSpecialtyById = (inputId, location) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId || !location) {
+                resolve({
+                    errCode: 1,
+                    message: 'Missing parameter'
+                })
+            } else {
+                let data = await db.Specialty.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ['descriptionHTML', 'descriptionMarkdown'], //những thuộc tính data cần lấy
+                })
+
+
+                if (data) {
+                    let doctorSpecialty = [];
+                    if (location === 'ALL') {
+                        doctorSpecialty = await db.Doctor_infor.findAll({
+                            where: { specialtyId: inputId },
+                            attributes: ['doctorId', 'provinceId'], //những thuộc tính data cần lấy
+                        })
+                    } else {
+                        //find by location
+                        doctorSpecialty = await db.Doctor_infor.findAll({
+                            where: {
+                                specialtyId: inputId,
+                                provinceId: location
+                            },
+                            attributes: ['doctorId', 'provinceId'], //những thuộc tính data cần lấy
+                        })
+                    }
+
+                    data.doctorSpecialty = doctorSpecialty
+                } else {
+                    data = {}
+                }
+
+                resolve({
+                    errCode: 0,
+                    message: 'Ok',
+                    data
+                })
+
+
+
+            }
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     createSpecialty: createSpecialty,
-    getAllSpecialty: getAllSpecialty
+    getAllSpecialty: getAllSpecialty,
+    getDetailSpecialtyById: getDetailSpecialtyById
 }
